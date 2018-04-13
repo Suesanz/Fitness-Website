ba00a146addbffb56adc90805816abb97ef3d0ec
suesanz <yadavsourav24071998@gmail.com>
2018-04-14 02:24:18 +0530

    updated
    Signed-off-by: suesanz <yadavsourav24071998@gmail.com>

--- db/models.js
+++ db/models.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const DataTypes = Sequelize.DataTypes
 
-const db = new Sequelize('passportdb', 'ppuser', 'root', {
+const db = new Sequelize('dea8mj5ini0l3o', 'clmtkuyzphqywi', 'b68e42452f48ca3cfd75f90893fa7f05a05e7a4b28fa272f4ed2e764dbd11833', {
     dialect: 'postgres'
 })
 
