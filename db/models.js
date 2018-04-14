6d9778c66529247c769c01c2f44ea3e30158c998
suesanz <yadavsourav24071998@gmail.com>
2018-04-14 14:24:26 +0530

    Updated
    Signed-off-by: suesanz <yadavsourav24071998@gmail.com>

--- db/models.js
+++ db/models.js
@@ -1,10 +1,20 @@
 const Sequelize = require('sequelize')
 const DataTypes = Sequelize.DataTypes
 
-const db = new Sequelize('dea8mj5ini0l3o', 'clmtkuyzphqywi', 'b68e42452f48ca3cfd75f90893fa7f05a05e7a4b28fa272f4ed2e764dbd11833', {
-    dialect: 'postgres'
-})
 
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+
+
+    const db = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+        dialect: 'postgres',
+        port: '5432',
+        host: 'ec2-54-235-193-34.compute-1.amazonaws.com'
+
+    })
+}
+else{
+    db=new Sequelize('passportdb','ppuser','root')
+}
 const User = db.define('users', {
     id: {
         type: DataTypes.INTEGER,
@@ -25,15 +35,15 @@ const UserFacebook = db.define('userfacebook', {
     photo: {type: Sequelize.DataTypes.STRING, allowNull: true},
 
 });
-const UserTwitter=db.define('usertwitter',{
-    id:{type:Sequelize.DataTypes.BIGINT,primaryKey:true},
-    accessToken:Sequelize.DataTypes.STRING,
-    refreshToken:{type:Sequelize.DataTypes.STRING,allowNull:true},
-    photo:{type:Sequelize.DataTypes.STRING,allowNull:true},
+const UserTwitter = db.define('usertwitter', {
+    id: {type: Sequelize.DataTypes.BIGINT, primaryKey: true},
+    accessToken: Sequelize.DataTypes.STRING,
+    refreshToken: {type: Sequelize.DataTypes.STRING, allowNull: true},
+    photo: {type: Sequelize.DataTypes.STRING, allowNull: true},
 });
 db.sync().then(() => "Database created")
 
 exports = module.exports = {
     db,
-    User,UserFacebook,UserTwitter
+    User, UserFacebook, UserTwitter
 }
--- server.js
+++ server.js
@@ -29,55 +29,55 @@ app.get('/', (r, s) => s.render('guest'))
 
 app.use('/ss',require('./routes/user'))
 const http = require('http')
-const socketio = require('socket.io')
-
-const server = http.createServer(app)
-const io = socketio(server)
-
-let socketIdName = {}
-
-io.on('connection', function (socket) {
-    console.log('Socket connected ' + socket.id)
-
-    socket.on('login', (data) => {
-        socketIdName[socket.id] = data.username
-        socket.join(data.username)
-
-        socket.emit('logged_in', {
-            username: data.username,
-            success: true
-        })
-    })
-
-    socket.on('chat', (data) => {
-        if (socketIdName[socket.id]) {
-
-            if (data.message.charAt(0) === '@') {
-
-                let recipient = data.message.split(' ')[0].substring(1)
-
-                io.to(recipient).emit('chat', {
-                    private: true,
-                    sender: socketIdName[socket.id],
-                    message: data.message,
-                    timestamp: new Date()
-                })
-
-            } else {
-                socket.broadcast.emit('chat', {
-                    sender: socketIdName[socket.id],
-                    message: data.message,
-                    timestamp: new Date()
-                })
-            }
-
-
-        }
-    })
-
-
-})
-
+// const socketio = require('socket.io')
+//
+// const server = http.createServer(app)
+// const io = socketio(server)
+//
+// let socketIdName = {}
+//
+// io.on('connection', function (socket) {
+//     console.log('Socket connected ' + socket.id)
+//
+//     socket.on('login', (data) => {
+//         socketIdName[socket.id] = data.username
+//         socket.join(data.username)
+//
+//         socket.emit('logged_in', {
+//             username: data.username,
+//             success: true
+//         })
+//     })
+//
+//     socket.on('chat', (data) => {
+//         if (socketIdName[socket.id]) {
+//
+//             if (data.message.charAt(0) === '@') {
+//
+//                 let recipient = data.message.split(' ')[0].substring(1)
+//
+//                 io.to(recipient).emit('chat', {
+//                     private: true,
+//                     sender: socketIdName[socket.id],
+//                     message: data.message,
+//                     timestamp: new Date()
+//                 })
+//
+//             } else {
+//                 socket.broadcast.emit('chat', {
+//                     sender: socketIdName[socket.id],
+//                     message: data.message,
+//                     timestamp: new Date()
+//                 })
+//             }
+//
+//
+//         }
+//     })
+//
+//
+// })
+//
 
 app.use('/premium/chat', express.static(__dirname + '../views/Sockets/public'))
 app.get('/premium/chat', (req, res) => {
